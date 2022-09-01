3056299777e0772c7461fd6585435535795f9afc
ana-lapas <anapaulalapas@gmail.com>
2022-08-31 23:36:24 -0400

    Layout tela 3

--- script.js
+++ script.js
@@ -28,3 +28,91 @@ function trocarTela(botaolecionado){
 /*FUNÇÕES E VARIÁVEIS RELACIONADOS A TELA 1*/
 /*FUNÇÕES E VARIÁVEIS RELACIONADOS A TELA 2*/
 /*FUNÇÕES E VARIÁVEIS RELACIONADOS A TELA 3*/
+/* 
+Criar função para repetir os campos de inputs conforme a quantidade de perguntas inseridas, e a quantidade de niveis também
+function verificacaoInput {
+    if (não estiverem todos preenchidos){
+        alert("Por favor, preencha os dados corretamente");
+        verificacaoInput();
+    }
+    adicionarDados();
+}
+função que confere se o quizz foi preenchido, se for preenchido -> armarzenar dados -> passar de página, talvez criar em 3 funções diferentes
+function adicionarDados(){
+    const dados = document.querySelector(...)
+    const quizzCriado = {
+	title: "Título do quizz",
+	image: "https://http.cat/411.jpg",
+	questions: [
+		{
+			title: "Título da pergunta 1",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 2",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 3",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		}
+	],
+	levels: [
+		{
+			title: "Título do nível 1",
+			image: "https://http.cat/411.jpg",
+			text: "Descrição do nível 1",
+			minValue: 0
+		},
+		{
+			title: "Título do nível 2",
+			image: "https://http.cat/412.jpg",
+			text: "Descrição do nível 2",
+			minValue: 50
+		}
+	]
+}
+
+salvarTodosDados()
+}
+function salvarTodosDados(){
+telasQuizz()
+}
+function telasQuizz() {
+}*/
